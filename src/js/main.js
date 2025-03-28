1cf6695d9dd7a4f688318a9a1837eb0ac0e27c64
Валерий <valery.shumkov@mail.ru>
2025-03-28 20:31:59 +0300

    DisableDevtool was added

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 import '../scss/style.scss';
 
+import DisableDevtool from 'disable-devtool';
 import { preloadImages } from './preload';
 import { startgame } from './gameplay/startgame';
 
 preloadImages((loadedImages) => {
   startgame();
 });
+
+// DisableDevtool({
+//   ondevtoolopen: () => {
+//     window.location.replace('https://t.me/voity_v_IT_2025');
+//   }
+// });
