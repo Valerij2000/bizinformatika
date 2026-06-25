ebd8e07e5723d14ed23fd61985695ef4cc84e5a9
Валерий <valery.shumkov@mail.ru>
2026-06-25 11:02:09 +0300

    final screen updated

--- src/js/templates/finalScreen.js
+++ src/js/templates/finalScreen.js
@@ -10,9 +10,18 @@ export const finalScreen = `
       </p>
     </div>
     <div class="final__btns">
-      <button id="startgame" class="btn green-btn">Вернуться в&nbsp;начало</button>
-      <a id="telegramBtn" href="https://t.me/voity_v_IT_2025" target="_blank" class="btn orange-btn">Перейти в
-        телеграм</a>
+      <button id="startgame" class="btn green-btn">Вернуться в&nbsp;начало</button>     
+      <a id="maxBtn" href="https://max.ru/join/eVtSiMV90WfLATEW9Pp2oUfq0IwjNkMqRDDZ3HnrdAU" target="_blank" class="btn orange-btn">
+        Перейти в MAX
+      </a>
+    </div>
+    <div class="social-bar">
+      <a href="https://t.me/voity_v_IT_2025" class="social-link" target="_blank">
+        <img src="./img/tg.png" alt="Telegram канал кафедры">
+      </a>
+      <a href="https://vk.com/business_inform_donnu" class="social-link" target="_blank">
+        <img src="./img/vk.png" alt="Группа ВКонтакте кафедры">
+      </a>
     </div>
     <button class="btn final__btn" id="downloadDoc">
       Скачать памятку <img class="final__tg" src="./img/download-ico.png" alt="">
